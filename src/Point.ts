e15d1ab00fc0689ffc41904d60537227bbaad3d2
Arthur Carabott <arthur@arthurcarabott.com>
2021-12-24 11:31:47 -0800

    convert Point

--- src/Point.ts
+++ src/Point.ts
@@ -1,9 +1,12 @@
 export class Point {
+  public x: number;
+  public y: number;
+
   static get zero() {
     return new Point(0, 0);
   }
 
-  constructor(x, y) {
+  constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
   }
@@ -13,10 +16,11 @@ export class Point {
     yield this.y;
   }
 
-  gte(point) {
+  gte(point: Point) {
     return this.x >= point.x && this.y >= point.y;
   }
-  lte(point) {
+
+  lte(point: Point) {
     return this.x <= point.x && this.y <= point.y;
   }
 
@@ -24,41 +28,41 @@ export class Point {
     return `${this.x.toFixed(0)}, ${this.y.toFixed(0)}`;
   }
 
-  subtract(pointOrX, y) {
-    const args =
+  subtract(pointOrX: Point | number, y: number) {
+    const args: [number, number] =
       pointOrX instanceof Point
         ? [this.x - pointOrX.x, this.y - pointOrX.y]
         : [this.x - pointOrX, this.y - y];
     return new Point(...args);
   }
 
-  add(pointOrX, y) {
-    const args =
+  add(pointOrX: Point | number, y: number) {
+    const args: [number, number] =
       pointOrX instanceof Point
         ? [this.x + pointOrX.x, this.y + pointOrX.y]
         : [this.x + pointOrX, this.y + y];
     return new Point(...args);
   }
 
-  mul(x, y = x) {
+  mul(x: number, y = x) {
     return new Point(this.x * x, this.y * y);
   }
 
-  divide(x, y = x) {
+  divide(x: number, y = x) {
     return new Point(this.x / x, this.y / y);
   }
 
-  distance(point) {
+  distance(point: Point) {
     const x = this.x - point.x;
     const y = this.y - point.y;
     return Math.sqrt(x * x + y * y);
   }
 
-  min(point) {
+  min(point: Point) {
     return new Point(Math.min(this.x, point.x), Math.min(this.y, point.y));
   }
 
-  max(point) {
+  max(point: Point) {
     return new Point(Math.max(this.x, point.x), Math.max(this.y, point.y));
   }
 
